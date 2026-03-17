88eb16139e3a38dc5bb50515c4ad1e0edbcce57d
Emanoel Becker <emanoelbc01@gmail.com>
2026-03-16 23:01:13 -0300

    primeiro commit

--- appEjs/routes/index.js
+++ appEjs/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Grama Santo Agostinho' });
+});
+
+/* GET predadores page. */
+router.get('/predadores', function(req, res, next) {
+  res.render('predadores', { title: 'Predadores' });
+});
+
+/* GET produtos page. */
+router.get('/produtos', function(req, res, next) {
+  res.render('produtos', { title: 'Produtos' });
 });
 
 module.exports = router;
--- appHbs/routes/index.js
+++ appHbs/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Morangos' });
+});
+
+/* GET receitas page. */
+router.get('/receitas', function(req, res, next) {
+  res.render('receitas', { title: 'Receitas com Morangos' });
+});
+
+/* GET fazendas page. */
+router.get('/fazendas', function(req, res, next) {
+  res.render('fazendas', { title: 'Maiores Fazendas de Morangos' });
 });
 
 module.exports = router;
